959e71d2ea2938672a1a70a828bda2429154f63a
AnnaZubakin <146950990+AnnaZubakin@users.noreply.github.com>
2023-10-24 15:20:47 +0300

    more results
    October 24

--- CalendarDayFinder/Controller/ViewController.swift
+++ CalendarDayFinder/Controller/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var findDay: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -22,26 +26,54 @@ class ViewController: UIViewController {
     }
     
     func findWeekDay() {
-        #warning("find day of the year on find tap")
-        print(dayTextField.text ?? "")
-        //Calendar
-        //dateComponents
-        //DateFormatter -> dateFormat
-        //Logic Calculation, it can't be 0
-        // present alert if something wrong
-        //Result is for presenting the Day of week
-        //Change Find to Clear (clear all text filed to "")
+        guard let dayText = dayTextField.text, let monthText = monthTextField.text, let yearText = yearTextField.text,
+            let day = Int(dayText), let month = Int(monthText), let year = Int(yearText) else {
+                // Show an alert for invalid input
+                let alert = UIAlertController(title: "Invalid Input", message: "Please enter valid numeric values for day, month, and year.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                return
+        }
+
+        // Validate the input (day should be between 1 and 31, month between 1 and 12)
+        if day < 1 || day > 31 || month < 1 || month > 12 {
+            let alert = UIAlertController(title: "Invalid Input", message: "Please enter valid values for day (1-31) and month (1-12).", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+
+        // Perform calculation and display the result
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+
+        if let date = calendar.date(from: dateComponents) {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEEE"
+            let dayOfWeekString = dateFormatter.string(from: date)
+            resultLabel.text = dayOfWeekString
+
+            // Change button text to "Clear"
+            findDay.setTitle("Clear", for: .normal)
+            dayTextField.text = ""
+            monthTextField.text = ""
+            yearTextField.text = ""
+        }
     }
+
+      
     
     @IBAction func findButtonTapped(_ sender: Any) {
         findWeekDay()
-    
     }
-    
+}
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "info" {
             // Get the new view controller using segue.destination.
             guard let vc = segue.destination as? InfoViewController
@@ -52,13 +84,8 @@ class ViewController: UIViewController {
             
         //    vc.info
         }
-        
-      
-        
     
     }
     
 
-
-}
 
